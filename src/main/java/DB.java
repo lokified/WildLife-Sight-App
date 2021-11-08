e5fd5f9c4734795b489997ad0d20e04f3df1dc09
lokified <sheldon.obonyo@student.moringaschool.com>
2021-11-08 22:16:19 +0300

    change credentials

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -2,5 +2,5 @@ import org.sql2o.*;
 
 public class DB {
 
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "moringa", "dammey5");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-3-92-119-83.compute-1.amazonaws.com:5432/dajecs4n2ei61q", "zlvgbdatujtega", "fb27a113378d67328859c67b6b74e16803bfe835ee71b0d2bf89a7ba7996ebdb");
 }
