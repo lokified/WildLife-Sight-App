b8339df4e7f9275a23dedcb07dffb05386973b53
lokified <sheldon.obonyo@student.moringaschool.com>
2021-11-08 21:24:15 +0300

    change type

--- src/main/java/Sighting.java
+++ src/main/java/Sighting.java
@@ -33,8 +33,8 @@ public class Sighting implements DatabaseManagement{
         return id;
     }
 
-    public String getTimeSeen() {
-        return String.format("%1$TD %1$TR", timeSeen);
+    public Timestamp getTimeSeen() {
+        return timeSeen;
     }
 
     @Override
