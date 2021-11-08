65c51a9d47e5b2413bb42a4ece9da421c221348c
lokified <sheldon.obonyo@student.moringaschool.com>
2021-11-08 23:36:06 +0300

    add ports to heroku

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,13 +2,23 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import static spark.Spark.*;
 
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
     public static void main(String[] args) {
+
+        port(getHerokuAssignedPort());
 
         staticFileLocation("/public");
 
